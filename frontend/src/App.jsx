effc0a7286316dfe625a05b32754b917c3631e58
IT22269652 <IT22269652@my.sliit.lk>
2025-08-19 08:55:21 +0530

    Add file paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Register from './pages/User/Register';
+import Login from './pages/User/login';
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;

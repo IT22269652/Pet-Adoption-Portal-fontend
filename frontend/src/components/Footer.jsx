7c77b4da4497475672129b6bc884dc82ba3e95cf
IT22269652 <IT22269652@my.sliit.lk>
2025-08-20 10:59:07 +0530

    change footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-white py-4 mt-auto">
+    <footer className="bg-blue-400 py-4 mt-auto">
       <div className="container mx-auto text-center px-4">
-        <p className="text-gray-600">&copy; {new Date().getFullYear()} DreamDog. All rights reserved.</p>
+        <p className="text-gray-800">&copy; {new Date().getFullYear()} DreamPet. All rights reserved.</p>
       </div>
     </footer>
   );
